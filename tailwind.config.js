366f5cdc0badf332b6555e98d052719d128902f2
eojindesu <eojin2820@gmail.com>
2025-11-16 21:43:52 +0900

    design: font-size 단위 통일

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,49 +41,49 @@ export default {
 				title01: [
 					"var(--title01-size)",
 					{
+						fontWeight: "var(--title01-weight)",
 						lineHeight: "var(--title01-line)",
 						letterSpacing: "var(--title01-track)",
-						fontWeight: "var(--title01-weight)",
 					},
 				],
 				title02: [
 					"var(--title02-size)",
 					{
+						fontWeight: "var(--title02-weight)",
 						lineHeight: "var(--title02-line)",
 						letterSpacing: "var(--title02-track)",
-						fontWeight: "var(--title02-weight)",
 					},
 				],
 				title03: [
 					"var(--title03-size)",
 					{
+						fontWeight: "var(--title03-weight)",
 						lineHeight: "var(--title03-line)",
 						letterSpacing: "var(--title03-track)",
-						fontWeight: "var(--title03-weight)",
 					},
 				],
 				title04: [
 					"var(--title04-size)",
 					{
+						fontWeight: "var(--title04-weight)",
 						lineHeight: "var(--title04-line)",
 						letterSpacing: "var(--title04-track)",
-						fontWeight: "var(--title04-weight)",
 					},
 				],
 				title05: [
 					"var(--title05-size)",
 					{
+						fontWeight: "var(--title05-weight)",
 						lineHeight: "var(--title05-line)",
 						letterSpacing: "var(--title05-track)",
-						fontWeight: "var(--title05-weight)",
 					},
 				],
 				title06: [
 					"var(--title06-size)",
 					{
+						fontWeight: "var(--title06-weight)",
 						lineHeight: "var(--title06-line)",
 						letterSpacing: "var(--title06-track)",
-						fontWeight: "var(--title06-weight)",
 					},
 				],
 
@@ -91,49 +91,49 @@ export default {
 				body01: [
 					"var(--body01-size)",
 					{
+						fontWeight: "var(--body01-weight)",
 						lineHeight: "var(--body01-line)",
 						letterSpacing: "var(--body01-track)",
-						fontWeight: "var(--body01-weight)",
 					},
 				],
 				body02: [
 					"var(--body02-size)",
 					{
+						fontWeight: "var(--body02-weight)",
 						lineHeight: "var(--body02-line)",
 						letterSpacing: "var(--body02-track)",
-						fontWeight: "var(--body02-weight)",
 					},
 				],
 				body03: [
 					"var(--body03-size)",
 					{
+						fontWeight: "var(--body03-weight)",
 						lineHeight: "var(--body03-line)",
 						letterSpacing: "var(--body03-track)",
-						fontWeight: "var(--body03-weight)",
 					},
 				],
 				body04: [
 					"var(--body04-size)",
 					{
+						fontWeight: "var(--body04-weight)",
 						lineHeight: "var(--body04-line)",
 						letterSpacing: "var(--body04-track)",
-						fontWeight: "var(--body04-weight)",
 					},
 				],
 				body05: [
 					"var(--body05-size)",
 					{
+						fontWeight: "var(--body05-weight)",
 						lineHeight: "var(--body05-line)",
 						letterSpacing: "var(--body05-track)",
-						fontWeight: "var(--body05-weight)",
 					},
 				],
 				body06: [
 					"var(--body06-size)",
 					{
+						fontWeight: "var(--body06-weight)",
 						lineHeight: "var(--body06-line)",
 						letterSpacing: "var(--body06-track)",
-						fontWeight: "var(--body06-weight)",
 					},
 				],
 
@@ -149,33 +149,33 @@ export default {
 				caption02: [
 					"var(--caption02-size)",
 					{
+						fontWeight: "var(--caption02-weight)",
 						lineHeight: "var(--caption02-line)",
 						letterSpacing: "var(--caption02-track)",
-						fontWeight: "var(--caption02-weight)",
 					},
 				],
 				caption03: [
 					"var(--caption03-size)",
 					{
+						fontWeight: "var(--caption03-weight)",
 						lineHeight: "var(--caption03-line)",
 						letterSpacing: "var(--caption03-track)",
-						fontWeight: "var(--caption03-weight)",
 					},
 				],
 				caption04: [
 					"var(--caption04-size)",
 					{
+						fontWeight: "var(--caption04-weight)",
 						lineHeight: "var(--caption04-line)",
 						letterSpacing: "var(--caption04-track)",
-						fontWeight: "var(--caption04-weight)",
 					},
 				],
 				caption05: [
 					"var(--caption05-size)",
 					{
+						fontWeight: "var(--caption05-weight)",
 						lineHeight: "var(--caption05-line)",
 						letterSpacing: "var(--caption05-track)",
-						fontWeight: "var(--caption05-weight)",
 					},
 				],
 
@@ -183,9 +183,9 @@ export default {
 				label01: [
 					"var(--label01-size)",
 					{
+						fontWeight: "var(--label01-weight)",
 						lineHeight: "var(--label01-line)",
 						letterSpacing: "var(--label01-track)",
-						fontWeight: "var(--label01-weight)",
 					},
 				],
 			},
