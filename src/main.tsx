bf2e4efbb1673e3707a59b51ebdea8de8e18a2bd
eojindesu <eojin2820@gmail.com>
2025-11-16 16:02:54 +0900

    init: index.css 글로벌 스타일 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "@/theme.css";
 import { router } from "@/routes";
 
 const queryClient = new QueryClient({
