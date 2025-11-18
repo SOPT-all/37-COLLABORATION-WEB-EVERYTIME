2afd955dae3dc0bafca362d6011d53d31c519a6a
hummingbbird <pop9814@kookmin.ac.kr>
2025-11-19 01:18:59 +0900

    feat: badge 생성 #36

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,4 +1,4 @@
-import LogoIcon from "@/assets/images/mark.svg?react";
+import LogoIcon from "@/assets/images/logo.svg?react";
 import { cn } from "@/utils/cn";
 
 const Header = () => {
@@ -9,12 +9,20 @@ const Header = () => {
 				"h-[7rem]",
 				"flex items-center justify-center",
 				"bg-white",
-				"shadow-md",
+				"border border-b-[0.5px] border-gray-400",
 				"fixed top-0 left-0",
-				"z-50",
+				"z-1",
 			)}
 		>
-			<LogoIcon />
+			<div className={cn("flex items-center justify-center", "left-4", "gap-[1.2rem]")}>
+				<button type="button">
+					<LogoIcon width={38} height={32} />
+				</button>
+				<div className={cn("items-left flex flex-col")}>
+					<span className={cn("body02 text-primary-red")}>에브리타임</span>
+					<span className={cn("label01 text-primary-black")}>솝트대</span>
+				</div>
+			</div>
 			<h1 className={cn("text-2xl", "font-bold", "text-gray-900")}>App Header</h1>
 		</header>
 	);
