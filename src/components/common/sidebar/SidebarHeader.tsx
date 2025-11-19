cf333586319d05ca648614d3168ab533b8f4050c
jyeon03 <bjlyeon2003@naver.com>
2025-11-19 17:30:27 +0900

    fix: 디자인 단위 수정

--- src/components/common/sidebar/SidebarHeader.tsx
+++ src/components/common/sidebar/SidebarHeader.tsx
@@ -2,7 +2,7 @@ import type { SidebarHeaderProps } from "@/types/sidebar";
 
 const SidebarHeader = ({ title, isMore, onClick }: SidebarHeaderProps) => {
 	return (
-		<header className="flex w-[28rem] items-center justify-between bg-gray-100 px-[1rem] py-4">
+		<header className="flex w-[28rem] items-center justify-between bg-gray-100 px-[1rem] py-[1.2rem]">
 			<h2 className="title07 text-sub-blue">{title}</h2>
 			{isMore && (
 				<button
