5cfea9e6185f73053eed6df619571e68d4b9eb8c
jm8468 <jmtoyplay1113@gmail.com>
2025-11-20 04:59:05 +0900

    update: theme.css 적용

--- src/components/MainPage/BoardItem.tsx
+++ src/components/MainPage/BoardItem.tsx
@@ -9,10 +9,10 @@ const BoardItem = ({ content, createdAt }: ListProps) => {
 			className={cn(
 				"flex items-center justify-between",
 				"p-[1rem]",
-				"bg-white hover:bg-gray-200",
+				"bg-white hover:bg-[var(--color-gray-200)]",
 				"h-[3.8rem] w-[38.8rem]",
 				"whitespace-nowrap",
-				"border-r border-b border-l border-gray-300",
+				"border-r border-b border-l border-[var(--color-gray-300)]",
 			)}
 		>
 			<p className={cn("body05 text-gray-700", "w-[28rem] truncate")}>{content}</p>
