2afb563d91c6af4eb94dd7491f4bf4f77b16f89e
jm8468 <jmtoyplay1113@gmail.com>
2025-11-21 06:53:02 +0900

    feat: 검색 결과 조회 API를 위한 인터페이스 타입 추가

---
+++ src/types/search.ts
@@ -0,0 +1,11 @@
+export interface SearchResultType {
+	category: string;
+	id: number;
+	title: string;
+	content: string;
+	likeCount: number;
+	commentCount: number;
+	createdAt: string;
+	isAnonymous: boolean;
+	nickname: string;
+}
