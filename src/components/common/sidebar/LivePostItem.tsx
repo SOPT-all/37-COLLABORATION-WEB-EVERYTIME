f321926ed28901cc1f338840eda2bbbd05da7108
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-20 23:19:27 +0900

    chore: cn 함수로 스타일 지정 #25

--- src/components/common/sidebar/LivePostItem.tsx
+++ src/components/common/sidebar/LivePostItem.tsx
@@ -23,22 +23,22 @@ const LivePostItem = ({ post }: LivePostItemProps) => {
 				"cursor-pointer border border-gray-400 bg-gray-100 hover:bg-white",
 			)}
 		>
-			<h3 className="title04 max-w-[26rem] truncate text-gray-800">{truncateByLength(post.title, 21)}</h3>
+			<h3 className={cn("title04 max-w-[26rem] truncate text-gray-800")}>{truncateByLength(post.title, 21)}</h3>
 
-			<p className="body06 text-gray-600">{post.content.slice(0, 42)}</p>
+			<p className={cn("body06 text-gray-600")}>{post.content.slice(0, 42)}</p>
 
-			<div className="flex items-center gap-[2.2rem]">
-				<span className="caption01 text-gray-500">{post.category}</span>
+			<div className={cn("flex items-center gap-[2.2rem]")}>
+				<span className={cn("caption01 text-gray-500")}>{post.category}</span>
 
-				<div className="flex items-center gap-[0.6rem]">
-					<div className="flex items-center gap-[0.2rem]">
-						<IconThumbsUp className="h-[1.1rem] w-[1.1rem]" />
-						<span className="caption04 text-primary-red">{post.likeCount}</span>
+				<div className={cn("flex items-center gap-[0.6rem]")}>
+					<div className={cn("flex items-center gap-[0.2rem]")}>
+						<IconThumbsUp className={cn("h-[1.1rem] w-[1.1rem]")} />
+						<span className={cn("caption04 text-primary-red")}>{post.likeCount}</span>
 					</div>
 
-					<div className="flex items-center gap-[0.2rem]">
-						<IconMyComment className="h-[1.1rem] w-[1.1rem]" />
-						<span className="caption04 text-sub-green">{post.commentCount}</span>
+					<div className={cn("flex items-center gap-[0.2rem]")}>
+						<IconMyComment className={cn("h-[1.1rem] w-[1.1rem]")} />
+						<span className={cn("caption04 text-sub-green")}>{post.commentCount}</span>
 					</div>
 				</div>
 			</div>
