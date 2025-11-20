afd528e7a95c1901b190943606db08fdf801e6a4
eojindesu <eojin2820@gmail.com>
2025-11-20 21:52:04 +0900

    chore: MainPage에  불필요한 SimplePostItem 임시 코드 삭제

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,5 @@
-import { SimplePostItem } from "@/components/common/sidebar/SimplePostItem";
-
 const MainPage = () => {
-	return (
-		<div className="relative">
-			<SimplePostItem title="하둘셋넷다하둘셋넷다하둘셋넷다하둘셋넷다" createdAt="2025-11-15T15:10:00" />
-		</div>
-	);
+	return <div className="relative"></div>;
 };
 
 export default MainPage;
