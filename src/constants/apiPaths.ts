51554ea4d6bf356fbb93dcaead64e40d681870ec
jm8468 <jmtoyplay1113@gmail.com>
2025-11-21 20:10:50 +0900

    feat: API 경로에 대한 상수 파일 생성

---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,8 @@
+export const END_URL = {
+	GET_REVIEWS: "reviews",
+	GET_POSTS: "/posts",
+	GET_POSTS_REALTIME: "/posts/realtime",
+	GET_POSTS_HOT: "/posts/hot",
+	GET_POSTS_SEARCH: "/posts/search",
+	GET_MARKET: "/market",
+};
