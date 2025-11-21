2208d93e5bd954f7174eb52db6176f0ec4ab147c
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-21 13:16:08 +0900

    refactor: LivePost content truncate->max-h 교체 #25

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,6 +7,8 @@ const MainPage = () => {
 	return (
 		<div className="relative">
 			<SimplePostItem title="하둘셋넷다하둘셋넷다하둘셋넷다하둘셋넷다" createdAt="2025-11-15T15:10:00" />
+			<ProfileItem nickname="전설의 맛" name="김경아" userId="ruddk0821" />
+
 			{/* 실시간 인기 글이 있는 경우 */}
 			<LivePostItem post={MOCK_LIVE_POST} />
 
