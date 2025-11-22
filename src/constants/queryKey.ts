0dd0735f548956767e82a9d3b986d4fb515d57e6
eojindesu <eojin2820@gmail.com>
2025-11-23 01:30:23 +0900

    feat: 쿼리 키 작성 #76

---
+++ src/constants/queryKey.ts
@@ -0,0 +1,11 @@
+export const QUERY_KEYS = {
+	REVIEWS: "reviews",
+
+	POSTS: "posts",
+	POSTS_REALTIME: "posts_realtime",
+	POSTS_HOT: "posts_hot",
+	POSTS_SEARCH: "posts_search",
+	POST_DETAIL: "post_detail",
+
+	MARKET: "market",
+};
