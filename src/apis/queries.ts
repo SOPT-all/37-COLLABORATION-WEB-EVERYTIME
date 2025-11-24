1cf9410a04b7eab9efd3be27bfc1c1498ac22b89
jm8468 <jmtoyplay1113@gmail.com>
2025-11-25 02:32:18 +0900

    feat: useGetPosts 훅 추가

--- src/apis/queries.ts
+++ src/apis/queries.ts
@@ -1,11 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getReviews } from "@/apis/api";
+import { getPosts, getReviews } from "@/apis/api";
 import { QUERY_KEYS } from "@/constants/queryKey";
 
 export const useGetReviews = () => {
 	return useQuery({
 		queryKey: [QUERY_KEYS.REVIEWS],
 		queryFn: getReviews,
+	});
+};
+
+export const useGetPosts = () => {
+	return useQuery({
+		queryKey: [QUERY_KEYS.POSTS],
+		queryFn: getPosts,
 	});
 };
