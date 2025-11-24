d63cbdaac47d3f7347c76f163c9fb0d1119a014f
jyeon03 <bjlyeon2003@naver.com>
2025-11-24 22:58:01 +0900

    feat: getMarket API 함수 및 useGetMarket 훅 추가

--- src/apis/queries.ts
+++ src/apis/queries.ts
@@ -1,11 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getReviews } from "@/apis/api";
+import { getMarket, getReviews } from "@/apis/api";
 import { QUERY_KEYS } from "@/constants/queryKey";
 
 export const useGetReviews = () => {
 	return useQuery({
 		queryKey: [QUERY_KEYS.REVIEWS],
 		queryFn: getReviews,
+	});
+};
+
+export const useGetMarket = () => {
+	return useQuery({
+		queryKey: [QUERY_KEYS.MARKET],
+		queryFn: getMarket,
 	});
 };
