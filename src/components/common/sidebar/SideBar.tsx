f6fbbdddfee3885fa123a659d2ea03c1c856036d
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-24 16:20:11 +0900

    refactor: useLocation 훅으로 페이지에 따라 사이드바 분기처리 #63

--- src/components/common/sidebar/SideBar.tsx
+++ src/components/common/sidebar/SideBar.tsx
@@ -10,8 +10,7 @@ import { ProfileItem } from "./ProfileItem";
 import { SidebarHeader } from "./SidebarHeader";
 import { SimplePostItem } from "./SimplePostItem";
 
-const SIDEBAR_VARIENT = {
-	/* Todo: 수정 */
+const SIDEBAR_VARIANT = {
 	main: {
 		showProfile: true,
 		showMyHistory: true,
@@ -24,17 +23,17 @@ const SIDEBAR_VARIENT = {
 	},
 } as const;
 
-type SidebarVariant = keyof typeof SIDEBAR_VARIENT;
+type SidebarVariant = keyof typeof SIDEBAR_VARIANT;
 
 interface SidebarProps {
 	variant: SidebarVariant;
 }
 
 function Sidebar({ variant }: SidebarProps) {
-	const { showProfile, showMyHistory, showAds } = SIDEBAR_VARIENT[variant];
+	const { showProfile, showMyHistory, showAds } = SIDEBAR_VARIANT[variant];
 
 	return (
-		<aside className={cn("w-[32rem]", "flex flex-col", "gap-[0.5rem]", "sticky", "top-[8rem]", "h-fit")}>
+		<aside className={cn("flex flex-col", "gap-[0.5rem]", "top-[8rem]")}>
 			{showProfile && showMyHistory && (
 				<>
 					<ProfileItem nickname={"전설의 맛"} name={"김솝트"} userId={"soptalien"} />
@@ -54,10 +53,10 @@ function Sidebar({ variant }: SidebarProps) {
 
 			<section>
 				<SidebarHeader title={"HOT 게시물"} isMore={true} />
-				<SimplePostItem title={"테스트1"} createdAt={"1111-11-11T11:11:11"} />
-				<SimplePostItem title={"테스트2"} createdAt={"1111-11-11T11:11:11"} />
-				<SimplePostItem title={"테스트3"} createdAt={"1111-11-11T11:11:11"} />
-				<SimplePostItem title={"테스트4"} createdAt={"1111-11-11T11:11:11"} />
+				<SimplePostItem title={"학교에 푸드트럭 왔대"} createdAt={"2025-11-11T23:11:11"} />
+				<SimplePostItem title={"다들 시험 공부"} createdAt={"2025-11-11T15:14:11"} />
+				<SimplePostItem title={"[홍보] 제 37회 학생대표자 선거 "} createdAt={"2025-11-09T10:21:11"} />
+				<SimplePostItem title={"단과대학 로고 공모전 안내"} createdAt={"2025-11-09T01:00:11"} />
 			</section>
 
 			<section>
@@ -66,9 +65,28 @@ function Sidebar({ variant }: SidebarProps) {
 
 			<section>
 				<SidebarHeader title={"최근 강의평"} isMore={true} />
-				<ReviewItem rate={3} lectureTitle={"테스트"} professorName={"테스트"} reviewContent={"테스트"} />
-				<ReviewItem rate={4} lectureTitle={"테스트2"} professorName={"테스트2"} reviewContent={"테스트2"} />
-				<ReviewItem rate={5} lectureTitle={"테스트3"} professorName={"테스트3"} reviewContent={"테스트3"} />
+				<ReviewItem
+					rate={3}
+					lectureTitle={"데이터베이스"}
+					professorName={"송충건"}
+					reviewContent={
+						"교수님 진짜 친절하시고 학생들 배려 많이 해주십니다. 근데 시험은 전부 서술형이라 꼼꼼하게 공부하시는게 좋아요."
+					}
+				/>
+				<ReviewItem
+					rate={5}
+					lectureTitle={"융합프로그래밍1"}
+					professorName={"한웅진"}
+					reviewContent={
+						"시험 난이도도 괜찮고 무엇보다 교수님 강의력이 정말 좋아요. 조교님도 실습시간때마다 잘 설명해주셔서 감사했습니다~"
+					}
+				/>
+				<ReviewItem
+					rate={1}
+					lectureTitle={"파이썬 프로그래밍"}
+					professorName={"조성균"}
+					reviewContent={"하.... 그냥 안습 ㅋㅋㅋ"}
+				/>
 			</section>
 
 			{showAds &&
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { Footer } from "@/components/common/footer/Footer";
 import { Header } from "@/components/common/header/Header";
+import { Sidebar } from "@/components/common/sidebar/SideBar";
 
 const Layout = () => {
+	const location = useLocation();
+
+	const sidebarVariant = () => {
+		if (location.pathname === "/search") {
+			return "search";
+		}
+		return "main";
+	};
+
 	return (
 		<div className="flex min-h-screen w-full flex-col">
 			<Header />
 			<main className="m-[7rem] flex-1">
+				<Sidebar variant={sidebarVariant()} />
 				<Outlet />
 			</main>
 			<Footer />
