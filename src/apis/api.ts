64242ee7a0ffb88ae63876d8e61ce86cad2fcbb2
jm8468 <jmtoyplay1113@gmail.com>
2025-11-25 02:32:58 +0900

    feat: getPosts API 함수 추가

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,12 @@
 import { get } from "@/apis/axiosInstance";
 import { END_URL } from "@/constants/url";
+import type { GetPostsResponse } from "@/types/getPostsResponse";
 import type { GetReviewsResponse } from "@/types/getReviewsResponse";
 
 export const getReviews = () => {
 	return get<GetReviewsResponse>(END_URL.GET_REVIEWS);
+};
+
+export const getPosts = () => {
+	return get<GetPostsResponse>(END_URL.GET_POSTS);
 };
