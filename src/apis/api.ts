85d02a1e297702eec91f3e9f6da66bc2d3468fc6
hummingbbird <pop9814@kookmin.ac.kr>
2025-11-25 15:30:45 +0900

    feat: api, query  구현 #96

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,17 @@
 import { get } from "@/apis/axiosInstance";
 import { END_URL } from "@/constants/url";
+import type { GetPostsSearchResponse } from "@/types/getPostsSearchResponse";
 import type { GetReviewsResponse } from "@/types/getReviewsResponse";
+import { changeLabelToCode } from "@/utils/changeLabelToCode";
 
 export const getReviews = () => {
 	return get<GetReviewsResponse>(END_URL.GET_REVIEWS);
+};
+
+export const getPostsSearch = (keyword: string, category: string, page: number) => {
+	const categoryCode = changeLabelToCode(category);
+	console.log("categoryCode: ", categoryCode); // TODO: 삭제 예정
+	return get<GetPostsSearchResponse>(
+		`${END_URL.GET_POSTS_SEARCH}?category=${categoryCode}&keyword=${encodeURIComponent(keyword)}&page=${page}`,
+	);
 };
--- src/apis/queries.ts
+++ src/apis/queries.ts
@@ -1,11 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getReviews } from "@/apis/api";
+import { getReviews, getPostsSearch } from "@/apis/api";
 import { QUERY_KEYS } from "@/constants/queryKey";
 
 export const useGetReviews = () => {
 	return useQuery({
 		queryKey: [QUERY_KEYS.REVIEWS],
 		queryFn: getReviews,
+	});
+};
+
+export const useGetPostsSearch = (keyword: string, category: string = "전체", page: number = 1) => {
+	return useQuery({
+		queryKey: [QUERY_KEYS.POSTS_SEARCH],
+		queryFn: () => getPostsSearch(keyword, category, page),
 	});
 };
