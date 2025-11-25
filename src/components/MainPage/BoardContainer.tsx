3c920675b58cacd8f1376fc8107ac352ab6f2229
jm8468 <jmtoyplay1113@gmail.com>
2025-11-25 15:44:23 +0900

    feat: BoardContainer 컴포넌트의 스켈레톤 뷰 추가

--- src/components/MainPage/BoardContainer.tsx
+++ src/components/MainPage/BoardContainer.tsx
@@ -2,10 +2,13 @@ import { useGetPosts } from "@/apis/queries";
 import type { PostsDataType } from "@/types/getPostsResponse";
 
 import { Board } from "./Board";
+import { BoardContainerSkeleton } from "./BoardContainerSkeleton";
 
 const BoardContainer = () => {
-	const { data } = useGetPosts();
+	const { data, isPending } = useGetPosts();
 	const boards = (data?.data ?? []) as PostsDataType[];
+
+	if (isPending) return <BoardContainerSkeleton />;
 
 	return (
 		<section aria-label="게시판 목록" className="grid grid-cols-2 gap-x-[0.4rem]">
