d97d8e16093d0e1f1cfeadb651babd1cc89ebc4d
jyeon03 <bjlyeon2003@naver.com>
2025-11-26 15:16:19 +0900

    fix: useSuspenseQuery로 변경

--- src/apis/queries.ts
+++ src/apis/queries.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 
 import { getMarket, getPostsRealtime, getPosts, getReviews } from "@/apis/api";
 import { QUERY_KEYS } from "@/constants/queryKey";
@@ -11,21 +11,21 @@ export const useGetReviews = () => {
 };
 
 export const useGetPosts = () => {
-	return useQuery({
+	return useSuspenseQuery({
 		queryKey: [QUERY_KEYS.POSTS],
 		queryFn: getPosts,
-  });
+	});
 };
 
 export const useGetMarket = () => {
-	return useQuery({
+	return useSuspenseQuery({
 		queryKey: [QUERY_KEYS.MARKET],
 		queryFn: getMarket,
 	});
 };
 
 export const useGetPostsRealtime = () => {
-	return useQuery({
+	return useSuspenseQuery({
 		queryKey: [QUERY_KEYS.POSTS_REALTIME],
 		queryFn: getPostsRealtime,
 		staleTime: 1000 * 30,
