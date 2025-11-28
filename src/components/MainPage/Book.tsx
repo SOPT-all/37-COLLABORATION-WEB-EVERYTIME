58b20e68ac38fa79f837c373001d9214544ae3a7
hummingbbird <pop9814@kookmin.ac.kr>
2025-11-28 11:39:21 +0900

    remove: 안 쓰는 코드 삭제 #144

--- src/components/MainPage/Book.tsx
+++ src/components/MainPage/Book.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
-
 import { useGetMarket } from "@/apis/queries";
-import Logo from "@/assets/images/logo.svg?react";
 import type { MarketDataType } from "@/types/getMarketResponse";
 import { cn } from "@/utils/cn";
 
@@ -11,18 +8,8 @@ const Book = () => {
 	const { data: marketResponse } = useGetMarket();
 	const bookList = (marketResponse?.data ?? []) as MarketDataType[];
 
-	const [imageErrors, setImageErrors] = useState<Map<number, boolean>>(new Map());
-
-	const handleImageError = (bookId: number) => {
-		setImageErrors((prev) => {
-			const newMap = new Map(prev);
-			newMap.set(bookId, true);
-			return newMap;
-		});
-	};
-
 	return (
-		<div className="flex gap-[0.5rem]">
+		<div className={cn("flex gap-[0.5rem]")}>
 			{bookList.map((book) => {
 				return <BookItem key={book.id} title={book.title} price={book.price} imgUrl={book.bookImgUrl} />;
 			})}
